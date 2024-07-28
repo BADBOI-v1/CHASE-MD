1cb6c5f78d7b039be8c1cab9b267aba36eda3992
BADBOI HACKER <165565859+BADBOI-v1@users.noreply.github.com>
2024-07-28 18:23:00 +0100

    Update rank.js

--- commandes/rank.js
+++ commandes/rank.js
@@ -134,7 +134,7 @@ zokou( {
     
     
          let msg = `
-┏━━⬡┃TKM bot┃⬡Rank⬡━━┓
+┏━━⬡┃CHASE bot┃⬡Rank⬡━━┓
          
     *Name :* @${auteurMsgRepondu.split("@")[0]}
     
@@ -210,7 +210,7 @@ zokou( {
     
     
          let msg = `
-┏━━⬡┃TKM bot┃⬡Rank⬡━━┓
+┏━━⬡┃CHASE bot┃⬡Rank⬡━━┓
      
   *Name :* ${nomAuteurMessage}
 
@@ -251,7 +251,7 @@ zokou( {
     const {ms , mybotpic} = commandeOptions ;
 
 
-       let msg = `┏━⬡┃TKM bot┃⬡Top-Rank━┓\n\n`
+       let msg = `┏━⬡┃CHASE bot┃⬡Top-Rank━┓\n\n`
        
       let topRanks = await getBottom10Users() ;
         let mention = [] ;
