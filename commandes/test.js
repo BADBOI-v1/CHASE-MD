e00c6e367a33b0fc57a65922ea0cc149ada0087c
BADBOI HACKER <165565859+BADBOI-v1@users.noreply.github.com>
2024-07-28 22:03:15 +0100

    Update test.js

--- commandes/test.js
+++ commandes/test.js
@@ -11,7 +11,7 @@ zokou(
             "i'm a whatsapp bot multi-device";
         let d = " by *BADBOI*";
         let varmess = z + d;
-        var img = "https://telegra.ph/file/e07a3d933fb4cad0b3791.jpg";
+        var img = "https://telegra.ph/file/6cad0ed0565f1edfc94c0.jpg";
         await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
         //console.log("montest")
     }
@@ -28,7 +28,7 @@ async function  execute  (origineMessage,zok) {
    let z ='Salut je m\'appelle *Zokou* \n\n '+'je suis un bot Whatsapp Multi-appareil '
       let d =' developpé par *Djalega++*'
       let varmess=z+d
-      var img='https://telegra.ph/file/2fc766ab7467ded0fac9c.png'
+      var img='https://telegra.ph/file/6cad0ed0565f1edfc94c0.jpg'
 await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
 }  */
 
