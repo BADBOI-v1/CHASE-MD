472ff3523278f171b66454698e303aea593b8a36
BADBOI HACKER <165565859+BADBOI-v1@users.noreply.github.com>
2024-07-28 21:28:52 +0100

    Update games.js

--- commandes/games.js
+++ commandes/games.js
@@ -150,7 +150,7 @@ zokou(
          let quizz = await axios.get("https://quizzapi.jomoreschi.fr/api/v1/quiz?limit=1&difficulty=facile") ;
 
          
-   let msg = `     ┃⬡TKM bot⬡quiz games┃
+   let msg = `     ┃⬡CHASE AND BOI bot⬡quiz games┃
 
 *Category :* ${ await traduire(quizz.data.quizzes[0].category , {to : 'en'})}
 *Question :* ${ await traduire(quizz.data.quizzes[0].question, {to : 'en'})}\n\n*Answers :*\n`
